22fe6db8ce28832b44d28717d081482555bf7614
atdalman <atdalman@gmail.com>
2020-08-11 14:38:54 +0900

    Test

---
+++ precache-manifest.0b606d234408eea21e0d2ffec4f559ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962017cbc663e22b823a56694e3c88f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58f694be07e1b3a7be92",
+    "url": "/static/css/main.023160f3.chunk.css"
+  },
+  {
+    "revision": "68d21ab8fd1cc9ba423a",
+    "url": "/static/js/2.e3e25ecb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3e25ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f694be07e1b3a7be92",
+    "url": "/static/js/main.e2f9619a.chunk.js"
+  },
+  {
+    "revision": "3fdd12bacebacf5ffb57",
+    "url": "/static/js/runtime-main.9f1852d2.js"
+  },
+  {
+    "revision": "063c1c7da40a8ea33a3457c3737c2e66",
+    "url": "/static/media/vga.063c1c7d.woff"
+  },
+  {
+    "revision": "2c3e089d0847907c32ec1c9c9169e05b",
+    "url": "/static/media/vga.2c3e089d.woff2"
+  },
+  {
+    "revision": "634410d4b532b9d0dd89fd916fbffe4e",
+    "url": "/static/media/vga.634410d4.ttf"
+  }
+]);
